5060fe651b7c5a145888d9dd15360defe7828bb9
AbbyBuchholz <abuchholz1@ufl.edu>
2025-10-25 19:14:07 -0400

    Added react.js and started on a web interface.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,9 +5,22 @@ function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+      <>
+          <body>
+          <header>
+              <h1>Trivia Game!</h1>
+          </header>
+
+          <content>
+          <main>
+              <p>Hello!</p>
+          </main>
+          <aside>
+              <p>Hi!</p>
+          </aside>
+          </content>
+          </body>
+      </>
   )
 }
 
