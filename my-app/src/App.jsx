dbd7e181343b70fedf0248757c0468c20bf82d5e
AbbyBuchholz <abuchholz1@ufl.edu>
2025-10-25 22:21:40 -0400

    Updated website

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
+  const [algoOneToggle] = useState(0)
+    const [algoTwoToggle] = useState(0)
+    const [points] = useState(0)
 
   return (
       <>
@@ -13,11 +15,39 @@ function App() {
 
           <content>
           <main>
-              <p>Hello!</p>
+              <div>
+              <p>Category:</p>
+              <div>Books</div>
+              <p>Points:</p>
+              <p>3</p>
+              </div>
+              <div id="question">
+                  <p>Hello!</p>
+              </div>
           </main>
-          <aside>
-              <p>Hi!</p>
-          </aside>
+              <aside>
+                  <p>Settings</p>
+                  <label className="switch">
+                      <input type="checkbox"/>
+                      <span className="slider round"></span>
+                  </label>
+                  <p>Merge Sort</p>
+                  <label className="switch">
+                      <input type="checkbox"/>
+                      <span className="slider round"></span>
+                  </label>
+                  <p>Heap Sort</p>
+                  <p>Select categories:</p>
+                  <div>Books, Movies, Songs, Cities</div>
+                  <p>Special Categorization:</p>
+
+                  <select name="verb" id="verb">
+                      <option value="Before">Before</option>
+                      <option value="After">After</option>
+                      <option value="Only">Only</option>
+                  </select>
+                  <input type="number" id="quantity" name="quantity" min="0" max="2025"></input>
+              </aside>
           </content>
           </body>
       </>
