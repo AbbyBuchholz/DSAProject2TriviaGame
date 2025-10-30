398b0bdbad3583eff7440d81cc50a8100bd69257
jedis <s.pappous@ufl.edu>
2025-10-30 13:54:21 -0400

    we be bridgin

---
+++ cmake-build-debug/CMakeFiles/DSAProject2TriviaGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSAProject2TriviaGame.dir/src/main.cpp.obj"
+  "CMakeFiles/DSAProject2TriviaGame.dir/src/main.cpp.obj.d"
+  "DSAProject2TriviaGame.exe"
+  "DSAProject2TriviaGame.exe.manifest"
+  "DSAProject2TriviaGame.pdb"
+  "libDSAProject2TriviaGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSAProject2TriviaGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
